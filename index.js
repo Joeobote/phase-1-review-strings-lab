b574a45a672a5239491d8a6cce0785417a386a14
JosephObote <joeobote01@gmail.com>
2023-07-11 22:25:44 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser=  "Grace Hoper"
+const welcomeMessage= 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+'Edsger Dijkstra'[0];
+//=> "E"
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
